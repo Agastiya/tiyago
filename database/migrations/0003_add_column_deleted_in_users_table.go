66bb2da22a75f9a8be56bf7a2ee901d1809a8e95
T.Putra  Agastiya <ageztya.putra@gmail.com>
2025-07-19 10:49:41 +0700

    Update Migrations

--- database/migrations/0003_add_column_deleted_in_users_table.go
+++ database/migrations/0003_add_column_deleted_in_users_table.go
@@ -2,28 +2,10 @@ package migrations
 
 import (
 	"github.com/agastiya/tiyago/models"
-	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
 func AddColumnDeletedInUsersTable(db *gorm.DB) {
-	// Add "deleted_by" column if it does not exist
-	if !db.Migrator().HasColumn(&models.User{}, "deleted_by") {
-		if err := db.Migrator().AddColumn(&models.User{}, "deleted_by"); err != nil {
-			log.Fatal().Msgf("[Migration - AddColumnDeletedByInUsersTable] Failed: %v", err)
-		}
-		log.Info().Msg("[Migration - AddColumnDeletedByInUsersTable] Successfully migrated!")
-	} else {
-		log.Info().Msg("[Migration - AddColumnDeletedByInUsersTable] Skipped (column already exists)")
-	}
-
-	// Add "deleted_at" column if it does not exist
-	if !db.Migrator().HasColumn(&models.User{}, "deleted_at") {
-		if err := db.Migrator().AddColumn(&models.User{}, "deleted_at"); err != nil {
-			log.Fatal().Msgf("[Migration - AddColumnDeletedAtInUsersTable] Failed: %v", err)
-		}
-		log.Info().Msg("[Migration - AddColumnDeletedAtInUsersTable] Successfully migrated!")
-	} else {
-		log.Info().Msg("[Migration - AddColumnDeletedAtInUsersTable] Skipped (column already exists)")
-	}
+	AddColumn(db, "AddColumnDeletedInUsersTable", &models.User{}, "deleted_by")
+	AddColumn(db, "AddColumnDeletedInUsersTable", &models.User{}, "deleted_at")
 }
