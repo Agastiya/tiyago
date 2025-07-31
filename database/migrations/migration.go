05ea9d91d45acff8009ad8a57d6add6c5a0582bb
T.Putra  Agastiya <ageztya.putra@gmail.com>
2025-07-31 11:21:49 +0700

    remove all migration for recreate

--- database/migrations/migration.go
+++ database/migrations/migration.go
@@ -1,45 +1,11 @@
 package migrations
 
 import (
-	"github.com/rs/zerolog/log"
 	"gorm.io/gorm"
 )
 
-func Run(db *gorm.DB) {
-	CreateUsersTable(db)
-	DropColumnStatusInUsersTable(db)
-	AddColumnDeletedInUsersTable(db)
+func Up(db *gorm.DB) {
 }
 
-func CreateTable(db *gorm.DB, name string, models any) {
-	if !db.Migrator().HasTable(&models) {
-		if err := db.Migrator().CreateTable(models); err != nil {
-			log.Fatal().Msgf("[Migration - %s] Failed: %v", name, err)
-		}
-		log.Info().Msgf("[Migration - %s] Successfully Migrate!", name)
-	} else {
-		log.Info().Msgf("[Migration - %s] Skipped (table already exists)", name)
-	}
-}
-
-func AddColumn(db *gorm.DB, name string, models any, column string) {
-	if !db.Migrator().HasColumn(&models, column) {
-		if err := db.Migrator().AddColumn(&models, column); err != nil {
-			log.Fatal().Msgf("[Migration - %s] Failed: %v", name, err)
-		}
-		log.Info().Msgf("[Migration - %s] Successfully migrated!", name)
-	} else {
-		log.Info().Msgf("[Migration - %s] Skipped (column already exists)", name)
-	}
-}
-
-func DropColumn(db *gorm.DB, name string, models any, column string) {
-	if db.Migrator().HasColumn(&models, column) {
-		if err := db.Migrator().DropColumn(models, column); err != nil {
-			log.Fatal().Msgf("[Migration - %s] Failed: %v", name, err)
-		}
-		log.Info().Msgf("[Migration - %s] Successfully Migrate!", name)
-	} else {
-		log.Info().Msgf("[Migration - %s] Skipped (column not exists)", name)
-	}
+func Down(db *gorm.DB) {
 }
