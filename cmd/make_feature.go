ec5a8a6882b452e139781eac250686e24f27316f
T.Putra  Agastiya <ageztya.putra@gmail.com>
2025-08-05 10:56:04 +0700

    create model

--- cmd/make_feature.go
+++ cmd/make_feature.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strings"
@@ -9,6 +10,85 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
+
+func (c *Cmd) MakeFeatureCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "make:feature [name]",
+		Short: "Create new feature",
+		Args:  cobra.ExactArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			name := args[0]
+			lowerName := strings.ToLower(name)
+			funcName := utils.ToFuncName(name)
+
+			// Create Model
+			modelFileName := fmt.Sprintf("models/%s.go", lowerName)
+			shouldWrite := true
+
+			if FileExists(modelFileName) {
+				answer := AskForConfirmation("This file already exists. Do you want to override it? (y/N): ")
+				shouldWrite = answer
+			}
+
+			if shouldWrite {
+				if err := RenderAndWriteFile(lowerName, funcName, "model", modelFileName); err != nil {
+					log.Fatal().Msgf("❌ Error writing file: %v", err)
+				}
+			}
+
+			// Create Dto
+			dtoFileName := fmt.Sprintf("dto/%s.go", lowerName)
+			if err := RenderAndWriteFile(lowerName, funcName, "dto", dtoFileName); err != nil {
+				log.Fatal().Msgf("%v", err)
+			}
+
+			// Create Repository
+			repoPath := fmt.Sprintf("repository/%s", lowerName)
+			if !FileExists(repoPath) {
+				if err := os.Mkdir(repoPath, 0755); err != nil {
+					log.Fatal().Msgf("❌ Error creating repository feature folder: %v", err)
+				}
+			}
+
+			repoName := fmt.Sprintf("%s/%s.go", repoPath, lowerName)
+			if err := RenderAndWriteFile(lowerName, funcName, "repository", repoName); err != nil {
+				log.Fatal().Msgf("%v", err)
+			}
+
+			repoModelName := fmt.Sprintf("%s/model.go", repoPath)
+			if err := RenderAndWriteFile(lowerName, funcName, "repository_model", repoModelName); err != nil {
+				log.Fatal().Msgf("%v", err)
+			}
+
+			// Create Service
+			servicePath := fmt.Sprintf("service/%s", lowerName)
+			if !FileExists(servicePath) {
+				if err := os.Mkdir(servicePath, 0755); err != nil {
+					log.Fatal().Msgf("❌ Error creating repository feature folder: %v", err)
+				}
+			}
+
+			serviceName := fmt.Sprintf("%s/%s.go", servicePath, lowerName)
+			if err := RenderAndWriteFile(lowerName, funcName, "service", serviceName); err != nil {
+				log.Fatal().Msgf("%v", err)
+			}
+
+			serviceModelName := fmt.Sprintf("%s/model.go", servicePath)
+			if err := RenderAndWriteFile(lowerName, funcName, "service_model", serviceModelName); err != nil {
+				log.Fatal().Msgf("%v", err)
+			}
+
+			// Create Controller
+			controllerName := fmt.Sprintf("controller/%s.go", lowerName)
+			if err := RenderAndWriteFile(lowerName, funcName, "controller", controllerName); err != nil {
+				log.Fatal().Msgf("%v", err)
+			}
+
+			log.Info().Msgf("✅ Feature %s Created", funcName)
+		},
+	}
+	return cmd
+}
 
 func RenderAndWriteFile(lowerName, funcName, templateName, fileName string) error {
 	content, err := utils.RenderTemplate(templateName, map[string]string{
@@ -31,72 +111,11 @@ func FileExists(filename string) bool {
 	return err == nil
 }
 
-func (c *Cmd) MakeFeatureCmd() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "make:feature [name]",
-		Short: "Create new feature",
-		Args:  cobra.ExactArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			name := args[0]
-			lowerName := strings.ToLower(name)
-			funcName := utils.ToFuncName(name)
+func AskForConfirmation(prompt string) bool {
+	fmt.Print(prompt)
+	reader := bufio.NewReader(os.Stdin)
+	answer, _ := reader.ReadString('\n')
+	answer = strings.ToLower(strings.TrimSpace(answer))
 
-			// Create Dto
-			dtoFileName := fmt.Sprintf("dto/%s.go", lowerName)
-			err := RenderAndWriteFile(lowerName, funcName, "dto", dtoFileName)
-			if err != nil {
-				log.Fatal().Msgf("%v", err)
-			}
-
-			// Create Repository
-			repoPath := fmt.Sprintf("repository/%s", lowerName)
-			if !FileExists(repoPath) {
-				if err := os.Mkdir(repoPath, 0755); err != nil {
-					log.Fatal().Msgf("❌ Error creating repository feature folder: %v", err)
-				}
-			}
-
-			repoName := fmt.Sprintf("%s/%s.go", repoPath, lowerName)
-			err = RenderAndWriteFile(lowerName, funcName, "repository", repoName)
-			if err != nil {
-				log.Fatal().Msgf("%v", err)
-			}
-
-			repoModelName := fmt.Sprintf("%s/model.go", repoPath)
-			err = RenderAndWriteFile(lowerName, funcName, "repository_model", repoModelName)
-			if err != nil {
-				log.Fatal().Msgf("%v", err)
-			}
-
-			// Create Service
-			servicePath := fmt.Sprintf("service/%s", lowerName)
-			if !FileExists(servicePath) {
-				if err := os.Mkdir(servicePath, 0755); err != nil {
-					log.Fatal().Msgf("❌ Error creating repository feature folder: %v", err)
-				}
-			}
-
-			serviceName := fmt.Sprintf("%s/%s.go", servicePath, lowerName)
-			err = RenderAndWriteFile(lowerName, funcName, "service", serviceName)
-			if err != nil {
-				log.Fatal().Msgf("%v", err)
-			}
-
-			serviceModelName := fmt.Sprintf("%s/model.go", servicePath)
-			err = RenderAndWriteFile(lowerName, funcName, "service_model", serviceModelName)
-			if err != nil {
-				log.Fatal().Msgf("%v", err)
-			}
-
-			// Create Controller
-			controllerName := fmt.Sprintf("controller/%s.go", lowerName)
-			err = RenderAndWriteFile(lowerName, funcName, "controller", controllerName)
-			if err != nil {
-				log.Fatal().Msgf("%v", err)
-			}
-
-			log.Info().Msgf("✅ Feature %s Created", funcName)
-		},
-	}
-	return cmd
+	return answer == "y" || answer == "yes"
 }
